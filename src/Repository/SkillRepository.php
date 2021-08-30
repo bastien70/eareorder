9034b7fcf2cc70cb2682831c084acd7597e570b2
Bastien <bastien.loughin@outlook.com>
2021-08-30 15:10:17 +0200

    Added missing findAll function

--- src/Repository/SkillRepository.php
+++ src/Repository/SkillRepository.php
@@ -9,7 +9,6 @@ use Doctrine\Persistence\ManagerRegistry;
 /**
  * @method Skill|null find($id, $lockMode = null, $lockVersion = null)
  * @method Skill|null findOneBy(array $criteria, array $orderBy = null)
- * @method Skill[]    findAll()
  * @method Skill[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class SkillRepository extends ServiceEntityRepository
@@ -17,6 +16,14 @@ class SkillRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Skill::class);
+    }
+
+    public function findAll()
+    {
+        return $this->createQueryBuilder('s')
+            ->orderBy('s.position', 'ASC')
+            ->getQuery()
+            ->getResult();
     }
 
     // /**
